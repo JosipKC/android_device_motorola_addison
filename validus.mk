bf143df32dcddd296f5ba3b9b8b676400dac39bc
JosipKC <d3hum4niz3d@gmail.com>
2018-05-16 23:39:31 +0200

    add bootanimation size

--- validus.mk
+++ validus.mk
@@ -16,6 +16,10 @@
 
 $(call inherit-product, device/motorola/addison/full_addison.mk)
 
+# Boot animation
+TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 1920
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := addison
 PRODUCT_NAME := validus_addison
